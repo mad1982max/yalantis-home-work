6b0116459c3fb7bbf725d31ca303ac11494f0316
Maks Bezrodnyi <maks.bezrodnyi@gmail.com>
2020-12-26 16:19:38 +0200

    width fixes

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -2,6 +2,7 @@ import { Switch, Route } from "react-router-dom";
 import ListOgGoods from "../ListOfGoods/ListOfGoods";
 import Error404 from "../Error404/Error404";
 import Product from "../Product/Product";
+import "./main.css";
 
 const Main = () => (
   <main>
